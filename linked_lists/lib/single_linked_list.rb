8187a3edbc2ec265daa38d94f498ad71980ac518
lady3bean <annehawleybrett@gmail.com>
2015-03-31 11:46:02 -0400

    passing tests for singly linked lists

--- linked_lists/lib/single_linked_list.rb
+++ linked_lists/lib/single_linked_list.rb
@@ -14,7 +14,88 @@ class Node
 end
 
 class SingleLinkedList
-  # your code goes here
+  attr_accessor :head
+
+  def initialize(first_element)
+    @head = Node.new(first_element)
+  end
+
+  def last
+    node = @head
+    while node.next_node
+      node = node.next_node
+    end
+    node
+  end
+  
+  def append(element)
+    node = Node.new(element)
+    last.next_node = node
+  end
+
+  def prepend(element)
+    node = Node.new(element)
+    node.next_node = @head
+    @head = node
+  end
+
+  def shift
+    old_head = @head
+    @head = @head.next_node
+    old_head 
+  end
+
+  def length
+    length = @head ? 1 : 0
+    node = @head
+    while node.next_node
+      length += 1
+      node = node.next_node
+    end
+    length
+  end
+
+  def find(value)
+    node = @head
+    while node
+      if node.value == value
+        return node
+      else
+        node = node.next_node
+      end
+    end
+    return nil
+  end
+
+  def reverse
+    list = SingleLinkedList.new(@head.value)
+    node = @head
+    while node.next_node
+      node = node.next_node
+      list.prepend(node.value)
+    end
+    list
+  end
+
+  def reverse!
+    last = nil
+    node = @head
+    while node.next_node
+      prior = node.next_node
+      node.next_node = last
+      last = node
+      node = prior
+    end
+    node.next_node = last
+    @head = node
+    self
+  end
+
+  def insert_after(node, new_node)
+    new_node.next_node = node.next_node
+    node.next_node = new_node
+    self   
+  end
 end
 
 =begin
